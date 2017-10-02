1ad33d0b4d3d7a082e59e09cbc9d9f5b4065877c
Nicholas MacArthur <n.j.macarthur@gmail.com>
2017-10-02 13:04:12 +1000

    altered Feed file

--- bake-mate/src/Bake-Mate/Feed.js
+++ bake-mate/src/Bake-Mate/Feed.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import Card from 'material-ui/Card';
+import {Card, CardTitle, CardText} from 'material-ui/Card';
+
+import './Feed.css';
 
 class Feed extends Component{
 
   render(){
     return(
-      <Card />
+      <Card className="Standard-Post">
+        <CardTitle>Yo!</CardTitle>
+        <CardText>This is the things</CardText>
+      </Card>
     );
   }
 
